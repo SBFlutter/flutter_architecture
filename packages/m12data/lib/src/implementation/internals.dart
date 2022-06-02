30fec8c8453e247a86ae9f43099cf9614a9fdb34
Pavel Bushmakin <pavelxbushmakin@gmail.com>
2022-06-03 01:10:30 +0300

    Small fix

--- packages/m12data/lib/src/implementation/internals.dart
+++ packages/m12data/lib/src/implementation/internals.dart
@@ -1,7 +1,7 @@
 import 'package:m12data/src/api/services.dart';
 import 'package:injectable/injectable.dart';
 
-@lazySingleton(as: HealthService)
+@LazySingleton(as: HealthService)
 class DummyService implements HealthService {
   @override
   bool getHealth() {
--- packages/m12data/lib/src/service_provider.config.dart
+++ packages/m12data/lib/src/service_provider.config.dart
@@ -7,14 +7,15 @@
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
+import 'api/services.dart' as _i3;
 import 'implementation/internals.dart'
-    as _i3; // ignore_for_file: unnecessary_lambdas
+    as _i4; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
 _i1.GetIt $initGetIt(_i1.GetIt get,
     {String? environment, _i2.EnvironmentFilter? environmentFilter}) {
   final gh = _i2.GetItHelper(get, environment, environmentFilter);
-  gh.lazySingleton<_i3.DummyService>(() => _i3.DummyService());
+  gh.lazySingleton<_i3.HealthService>(() => _i4.DummyService());
   return get;
 }
